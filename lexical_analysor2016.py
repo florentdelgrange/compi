b07815471cb0331531695af9ba476a46c6b6ce2b
Clément Tamines <clement.tamines@gmail.com>
2016-05-15 13:53:35 +0100

    Changed hook open so both hooks are in one lexem. Added state to ignore TXT inside dumbo block. Ignoring line breaks and spaces inside dumbo blocks

--- lexical_analysor2016.py
+++ lexical_analysor2016.py
@@ -2,123 +2,132 @@ import ply.lex as lex
 
 tokens = ('LT','BT','EQ','NE','IF','ENDIF','TRUE','FALSE','OR','AND','ADDOP','MULOP','INTEGER','TXT', 'VARIABLE', 'STRING', 'HOOK_OPEN', 'HOOK_CLOSE', 'SEMICOLON', 'PRINT', 'FOR', 'IN', 'DO', 'ENDFOR', 'EQUALS', 'DOT', 'COMMA', 'PARENTHESIS_OPEN', 'PARENTHESIS_CLOSE')
 
-t_TXT = r'[a-z|A-Z|0-9|;|&|<|>|"|_|\-|\.|\\|\/|\n|\p|:|,|=]+'
+states = (
+( 'inBlock' , 'inclusive' ) ,
+)
 
 def t_HOOK_OPEN(t):
-    r'\{(\n)*'
+    r'\{\{(\n)*'
+    t.lexer.begin('inBlock')
     return t
 
-def t_HOOK_CLOSE(t):
-    r'\}(\n)*'
+def t_inBlock_HOOK_CLOSE(t):
+    r'\}\}(\n)*'
+    t.lexer.begin('INITIAL')
     return t
 
-def t_PARENTHESIS_CLOSE(t):
+def t_inBlock_PARENTHESIS_CLOSE(t):
     r'\)'
     return t
 
-def t_PARENTHESIS_OPEN(t):
+def t_inBlock_PARENTHESIS_OPEN(t):
     r'\('
     return t
 
-def t_COMMA(t):
+def t_inBlock_COMMA(t):
     r'\,'
     return t
 
-def t_DOT(t):
+def t_inBlock_DOT(t):
     r'\.'
     return t
 
-def t_ENDFOR(t):
+def t_inBlock_ENDFOR(t):
     r'endfor'
     return t
 
-def t_DO(t):
+def t_inBlock_DO(t):
     r'do(\n)*'
     return t
 
-def t_IN(t):
+def t_inBlock_IN(t):
     r'in'
     return t
 
-def t_FOR(t):
+def t_inBlock_FOR(t):
     r'for'
     return t
 
-def t_PRINT(t):
+def t_inBlock_PRINT(t):
     r'print'
     return t
 
-def t_SEMICOLON(t):
+def t_inBlock_SEMICOLON(t):
     r';(\n)*'
     return t
 
-def t_EQUALS(t):
+def t_inBlock_EQUALS(t):
     r':='
     return t
 
-def t_ADDOP(t):
+def t_inBlock_ADDOP(t):
     r'\+|\-'
     return t
 
-def t_MULOP(t):
+def t_inBlock_MULOP(t):
     r'\*|\/'
     return t
 
-def t_TRUE(t):
+def t_inBlock_TRUE(t):
     r'true'
     return t
 
-def t_FALSE(t):
+def t_inBlock_FALSE(t):
     r'false'
     return t
 
-def t_AND(t):
+def t_inBlock_AND(t):
     r'and'
     return t
 
-def t_OR(t):
+def t_inBlock_OR(t):
     r'or'
     return t
 
-def t_IF(t):
+def t_inBlock_IF(t):
     r'if'
     return t
 
-def t_ENDIF(t):
+def t_inBlock_ENDIF(t):
     r'endif'
     return t
 
-def t_LT(t):
+def t_TXT(t):
+    r'[a-z|A-Z|0-9|;|&|<|>|"|_|\-|\.|\\|\/|\n|\p|:|,|=]+'
+    return t
+
+def t_inBlock_LT(t):
     r'<'
     return t
 
-def t_BT(t):
+def t_inBlock_BT(t):
     r'>'
     return t
 
-def t_EQ(t):
+def t_inBlock_EQ(t):
     r'='
     return t
 
-def t_NE(t):
+def t_inBlock_NE(t):
     r'!='
     return t
 
-def t_STRING(t):
+def t_inBlock_STRING(t):
     r'\'[a-z|A-Z|0-9|;|&|<|>|"|_|\s|\-|\.|\\|\/|\n|\p|:|,|=]+\''
     t.value = str(t.value)
     return t
 
-def t_INTEGER(t):
+def t_inBlock_INTEGER(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
-def t_VARIABLE(t):
+def t_inBlock_VARIABLE(t):
     r'[a-z|A-Z|0-9_]+'
     return t
 
 t_ignore = ' \t'
+t_inBlock_ignore = ' |\n|\t'
 
 def t_error(t):
     print "Illegal character '%s'" %t.value[0]
